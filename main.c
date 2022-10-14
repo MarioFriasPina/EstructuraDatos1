911b1cfe98c63ade562e21b67094bf0bcb739837
Mario Frias <a01782559@tec.mx>
2022-10-14 12:38:59 -0500

    Added main function with test case

--- main.c
+++ main.c
@@ -2,5 +2,24 @@
 
 int main(void)
 {
-	return (1);
+	t_pqueue	*q;
+	t_circular	*list;
+
+	q = initPqueue();
+	enPqueue(q, 12, 1);
+	enPqueue(q, 14, 3);
+	enPqueue(q, 16, 2);
+	enPqueue(q, 11, 1);
+	enPqueue(q, 19, 1);
+	printPriorityQueue(q);
+
+	list = initCircular();
+	addCircular(list, 12);
+	addCircular(list, 15);
+	addCircular(list, 11);
+	addCircular(list, 19);
+	printCircular(list);
+	freePqueue(q);
+	freeCircular(list);
+	return (0);
 }
